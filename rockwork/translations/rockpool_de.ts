a42fda804068822952f285952f877dbeaac63c81
Allan Nordhøy <epost@anotheragency.no>
2017-11-11 12:52:23 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (225 of 225 strings)

--- rockwork/translations/rockpool_de.ts
+++ rockwork/translations/rockpool_de.ts
@@ -297,7 +297,7 @@
     <message>
         <location filename="../qml/pages/DeveloperToolsPage.qml" line="233"/>
         <source>Current log: </source>
-        <translation>Aktuelles Protokoll</translation>
+        <translation>Aktuelles Protokoll: </translation>
     </message>
     <message>
         <location filename="../qml/pages/DeveloperToolsPage.qml" line="239"/>
