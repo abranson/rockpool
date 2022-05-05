ff4e16f6f1fa2150b26dc8f84a41221ffaeab8ff
Andrew Branson <andrew.branson@jolla.com>
2022-05-05 21:48:36 +0200

    More mkcal changes

--- rockworkd/platformintegration/sailfish/organizeradapter.h
+++ rockworkd/platformintegration/sailfish/organizeradapter.h
@@ -29,7 +29,6 @@ public slots:
 
 protected:
     void storageModified(mKCal::ExtendedStorage *storage, const QString &info) Q_DECL_OVERRIDE;
-    void storageProgress(mKCal::ExtendedStorage *storage, const QString &info) Q_DECL_OVERRIDE;
     void storageFinished(mKCal::ExtendedStorage *storage, bool error, const QString &info) Q_DECL_OVERRIDE;
     void timerEvent(QTimerEvent *event) override;
 
